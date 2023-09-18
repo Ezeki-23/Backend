0d11fe7ceb9c9236f5a6c7f266cdc888ca6230b5
Ezeki-23 <ezekiel202x@gmail.com>
2023-09-17 21:00:26 -0300

    Se agrega el backend

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";   
+import morgan from 'morgan'; 
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+import authRoutes from './routes/auth.routes.js';
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'get, put, post, delete');
+    next();
+})
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173'
+})
+);
+
+app.use(morgan('dev')); 
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api", authRoutes);
+
+export default app;
