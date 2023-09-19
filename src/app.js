67ca878674f40e72440e04b3e39db9d625a1d5ad
Ezeki-23 <137580645+Ezeki-23@users.noreply.github.com>
2023-09-18 23:44:47 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,11 +13,7 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:5173'
-})
-);
+app.use(cors());
 
 app.use(morgan('dev')); 
 app.use(express.json());
