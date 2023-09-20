a729d704c85ba52815678e8fa0798c7925e65b27
Ezeki-23 <137580645+Ezeki-23@users.noreply.github.com>
2023-09-20 00:24:30 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,26 +7,7 @@ import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['https://ezekiel-dev.netlify.app']);
-    res.append('Access-Control-Allow-Methods', 'get, put, post, delete');
-    res.append('Access-Control-Allow-Credentials', true);
-    next();
-})
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', ['https://ezekiel-dev.netlify.app']);
-  res.setHeader('Access-Control-Allow-Methods', 'get, put, post, delete');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-})
-
-app.use(cors({
-    credentials: true,
-    origin: 'https://ezekiel-dev.netlify.app'
-})
-);
+app.use(cors());
 
 app.use(morgan('dev')); 
 app.use(express.json());
