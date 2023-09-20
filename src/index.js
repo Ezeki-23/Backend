c6b5c7788be9897e43de3543ba58f81a5ec8c4cb
Ezeki-23 <137580645+Ezeki-23@users.noreply.github.com>
2023-09-19 21:41:54 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import app from './app.js'
 import {connectDB} from './db.js'
 
+const cors = require('cors');
+
 connectDB(); 
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 
 app.listen(3000)
 console.log('Server on port', 3000)
