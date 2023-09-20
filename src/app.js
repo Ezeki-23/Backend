455c9b1e9796b3c259aaeb4a7996b7e025270445
Ezeki-23 <137580645+Ezeki-23@users.noreply.github.com>
2023-09-19 21:33:18 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 })
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', ['*']);
   res.setHeader('Access-Control-Allow-Methods', 'get, put, post, delete');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
