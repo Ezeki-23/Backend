b70749a27da8ede197a15a9a45323433344fc68d
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-09-21 19:52:41 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,10 +14,7 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use(cors({
-    credentials: true,
-    origin: 'https://ezekiel-dev.netlify.app'
-}));
+app.use(cors());
 
 app.use(morgan('dev')); 
 app.use(express.json());
