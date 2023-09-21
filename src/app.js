81645b6084d3d8486b4a8b1b8516527f5e189008
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-09-21 19:49:20 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,10 +8,9 @@ import authRoutes from './routes/auth.routes.js';
 const app = express();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', ['*']);
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Credentials', true);
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.append('Access-Control-Allow-Credentials', true);
     next();
 })
 
