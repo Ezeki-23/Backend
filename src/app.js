a5fa432144d4bcfb9057102ebad1ab1dfb48d9bb
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-09-21 20:42:40 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,17 +8,6 @@ import authRoutes from './routes/auth.routes.js';
 const app = express();
 
 app.use(cors());
-
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', 'https://ezekiel-dev.netlify.app');
-    res.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.append("Access-Control-Allow-Headers", "Content-Type");
-    res.append('Access-Control-Allow-Credentials', 'true');
-    next();
-})
-
-
-
 app.use(morgan('dev')); 
 app.use(express.json());
 app.use(cookieParser());
