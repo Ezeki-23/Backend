ca1466faa2a030658f2d0d69724aeda2c5381dc8
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-09-21 22:19:29 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,6 @@
 import app from './app.js'
 import {connectDB} from './db.js'
 
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://ezekiel-dev.netlify.app"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, PUT, POST, DELETE, OPTIONS, CONNECT, TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
-
-app.use(cors({
-  allowedHeader: "*",
-  allowMethods: "*",
-  origin: "*",
-}));
-app.use(express.json());
-
 connectDB(); 
 
 app.listen(3000)
