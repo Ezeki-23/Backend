a51015a733aacb9d901ef697adc338e5092bda5e
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-09-21 21:16:20 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,26 @@ const cors = require('cors');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://ezekiel-dev.netlify.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, DELETE, OPTIONS, CONNECT, TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 
