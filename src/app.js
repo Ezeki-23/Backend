9c86055767f594f1c2d46bbd2ed206e355212f00
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-09-21 22:22:45 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,31 +7,16 @@ import authRoutes from './routes/auth.routes.js';
 const app = express();
 
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://ezekiel-fullstack.vercel.app"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, PUT, POST, DELETE, OPTIONS, CONNECT, TRACE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
+  res.header("Access-Control-Allow-Origin", "https://ezekiel-dev.netlify.app");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers");
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Private-Network", true);
-  res.header("Access-Control-Max-Age", 7200);
-
   next();
 });
 
 app.use(cors({
-  allowedHeader: "Access-Control-Allow-Origin",
-  allowHeader: "Access-Control-Allow-Origin",
-  allowMethods: "*",
   credentials: true,
-  origin: "https://ezekiel-fullstack.vercel.app",
+  origin: "https://ezekiel-dev.netlify.app"
 }));
 
 app.use(morgan('dev')); 
