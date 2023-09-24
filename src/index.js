e5ea9d333295cb96689b096b5d35f4d92cb78e5c
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-09-24 00:46:23 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import {connectDB} from './db.js'
 
 connectDB(); 
 
+const cors = require('cors');    
+app.use(cors({credentials: true, origin: 'https://ezekiel-dev.netlify.app/'}));
+
 app.listen(3000)
 console.log('Server on port', 3000)
