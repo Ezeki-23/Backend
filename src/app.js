6f95d2d4cd5459f355ff39b5c872f750bcb7f304
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-09-24 00:35:53 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://ezekiel-dev.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers");
+  res.setheader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers");
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
