28f54629f629aef8a78d4857273a558e733e2e03
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-09-24 14:59:58 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 import app from './app.js'
-import {connectDB} from './db.js'
-import express from "express";   
+import {connectDB} from './db.js' 
 
 connectDB(); 
-
-const express = require('express');
-const app = express();
-const cors = require('cors');   
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  next();
-});
-
-app.use(cors({credentials: true, origin: 'https://ezekiel-dev.netlify.app'}));
 
 app.listen(3000)
 console.log('Server on port', 3000)
