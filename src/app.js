a781a8091efb209539434d4ade323d9f503668a3
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-09-24 15:30:45 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,12 +6,32 @@ import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', 'https://ezekiel-dev.netlify.app/');
-    res.append('Access-Control-Allow-Methods', 'get, put, post, delete');
-    next();
+app.use(function(req, res, next) {
+    var oneof = false;
+    if(req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
     }
-)
+    if(req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if(oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
+
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
 
 app.use(cors({
     credentials: true, 
