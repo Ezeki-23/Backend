cc50a20050b5c37962172ff478a80f39cc52ad94
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2023-09-26 21:38:41 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import app from './app.js'
 import {connectDB} from './db.js' 
 
-const express = require('express');
-const app = express();
-const cors = require('cors')
-
-app.use(cors({
-    credentials: true,
-    origin: 'https://ezekiel-dev.netlify.app'
-}));
-
 connectDB(); 
 
 app.listen(3000)
